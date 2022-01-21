8768cc6e0caf9d7d438c12a53d107df6e18de948
alphadzingina <alphaadx6@gmail.com>
2022-01-21 02:16:24 +0100

    display user and computer choices

--- main.py
+++ main.py
@@ -26,12 +26,15 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+choices_text = ["rock", "paper", "scissors"]
 
 print("Welcome to Rock-Paper-Scissors!")
 
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors:\n"))
+print("you chose " + choices_text[user_choice])
 
 computer_choice = random.randint(0,2)
+print("Computer chose " + choices_text[computer_choice])
 
 if user_choice >= 3 or user_choice < 0: 
   print("You typed an invalid number, you lose!") 
