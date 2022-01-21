dcf82b4a91000a64c5524a6e86a18300a5ee6fd7
alphadzingina <alphaadx6@gmail.com>
2022-01-21 02:44:12 +0100

    display choice images

--- main.py
+++ main.py
@@ -26,27 +26,34 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-choices_text = ["rock", "paper", "scissors"]
+choices_text = ["Rock", "Paper", "Scissors"]
+choices_image = [rock, paper, scissors]
 
 print("Welcome to Rock-Paper-Scissors!")
 
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors:\n"))
 print("you chose " + choices_text[user_choice])
+print(choices_image[user_choice])
 
 computer_choice = random.randint(0,2)
 print("Computer chose " + choices_text[computer_choice])
+print(choices_image[computer_choice])
 
 if user_choice >= 3 or user_choice < 0: 
   print("You typed an invalid number, you lose!") 
 elif user_choice == computer_choice:
-  print("It\'s a tie. play again")
+  print("It\'s a tie. Play again")
 elif user_choice == 0 and computer_choice == 1:
-  print("Paper Covers Rock. You lose!")
+  print("Computer\'s Paper covers your Rock. You Lose!")
 elif user_choice == 0 and computer_choice == 2:
-  print("Rock Crushes Scissors. You Win!")
+  print("Your Rock crushes Computer\'s Scissors. You Win!")
+elif user_choice == 1 and computer_choice == 0:
+  print("Your Paper covers Computer\'s Rock. You Win!")
 elif user_choice == 1 and computer_choice == 2:
-  print("Scissors Cuts Paper. You lose!")
-
-
+  print("Computer\'s Scissors cuts your Paper. You Lose!")
+elif user_choice == 2 and computer_choice == 0:
+  print("Computer's Rock crushes your Scissors. You Lose!")
+elif user_choice == 2 and computer_choice == 1:
+  print("Your Scissors cuts Computer\'s Paper. You Win!")
 
 
